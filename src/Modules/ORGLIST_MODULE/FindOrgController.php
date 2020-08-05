ad726aedc1c98868cb58fbf94490a48e5eb438a9
Nadyita <nadyita@hodorraid.org>
2020-08-05 14:54:47 +0200

    URL-format for PORK responses has changed, switched to async orglist pulling

--- src/Modules/ORGLIST_MODULE/FindOrgController.php
+++ src/Modules/ORGLIST_MODULE/FindOrgController.php
@@ -114,7 +114,63 @@ class FindOrgController {
 		}
 		return $blob;
 	}
-	
+
+	public function handleOrglistResponse($url, $searchIndex, $response) {
+		$search = $this->searches[$searchIndex];
+		$pattern = '@<tr>\s*'.
+			'<td align="left">\s*'.
+				'<a href="(?:https?:)?//people.anarchy-online.com/org/stats/d/(\d+)/name/(\d+)">\s*'.
+					'([^<]+)'.
+				'</a>'.
+			'</td>\s*'.
+			'<td align="right">(\d+)</td>\s*'.
+			'<td align="right">(\d+)</td>\s*'.
+			'<td align="left">([^<]+)</td>\s*'.
+			'<td align="left">([^<]+)</td>\s*'.
+			'<td align="left" class="dim">RK\d+</td>\s*'.
+			'</tr>@s';
+
+		try {
+			preg_match_all($pattern, $response->body, $arr, PREG_SET_ORDER);
+			$this->logger->log("DEBUG", "Updating orgs starting with $search");
+			$this->db->beginTransaction();
+			if ($search === 'others') {
+				if ($this->db->getType() === $this->db::MYSQL) {
+					$this->db->exec("DELETE FROM organizations WHERE name NOT REGEXP '^[a-zA-Z0-9]'");
+				} else {
+					$this->db->exec("DELETE FROM organizations WHERE name NOT GLOB '[a-zA-Z0-9]*'");
+				}
+			} else {
+				$this->db->exec("DELETE FROM organizations WHERE name LIKE ?", "{$search}%");
+			}
+			foreach ($arr as $match) {
+				$obj = new stdClass();
+				$obj->id = (int)$match[2];
+				$obj->name = trim($match[3]);
+				$obj->num_members = (int)$match[4];
+				$obj->faction = $match[6];
+			
+				$this->db->exec("INSERT INTO organizations (id, name, faction, num_members) VALUES (?, ?, ?, ?)", $obj->id, $obj->name, $obj->faction, $obj->num_members);
+			}
+			$this->db->commit();
+			$searchIndex++;
+			if ($searchIndex >= count($this->searches)) {
+				$this->logger->log("INFO", "Finished downloading orgs");
+				return;
+			}
+			$this->http
+				->get($url)
+				->withQueryParams(['l' => $this->searches[$searchIndex]])
+				->withTimeout(60)
+				->withCallback(function($response) use ($url, $searchIndex) {
+					$this->handleOrglistResponse($url, $searchIndex, $response);
+				});
+		} catch (Exception $e) {
+			$this->logger->log("ERROR", "Error downloading orgs");
+			$this->db->rollback();
+		}
+	}
+
 	/**
 	 * @Event("timer(24hrs)")
 	 * @Description("Parses all orgs from People of Rubi Ka")
@@ -122,44 +178,14 @@ class FindOrgController {
 	public function parseAllOrgsEvent(Event $eventObj) {
 		$url = "http://people.anarchy-online.com/people/lookup/orgs.html";
 		
-		$this->logger->log("DEBUG", "Downloading all orgs from '$url'");
-		try {
-			$this->db->beginTransaction();
-			$this->db->exec("DELETE FROM organizations");
-			foreach ($this->searches as $search) {
-				$response = $this->http->get($url)->withQueryParams(array('l' => $search))->waitAndReturnResponse();
-			
-				$pattern = '@<tr>\s*'.
-					'<td align="left">\s*'.
-						'<a href="http://people.anarchy-online.com/org/stats/d/(\d+)/name/(\d+)">\s*'.
-							'([^<]+)'.
-						'</a>'.
-					'</td>\s*'.
-					'<td align="right">(\d+)</td>\s*'.
-					'<td align="right">(\d+)</td>\s*'.
-					'<td align="left">([^<]+)</td>\s*'.
-					'<td align="left">([^<]+)</td>\s*'.
-					'<td align="left" class="dim">RK5</td>\s*'.
-					'</tr>@s';
-
-				preg_match_all($pattern, $response->body, $arr, PREG_SET_ORDER);
-				foreach ($arr as $match) {
-					$obj = new stdClass;
-					//$obj->server = $match[1]; unused
-					$obj->id = $match[2];
-					$obj->name = trim($match[3]);
-					$obj->num_members = $match[4];
-					$obj->faction = $match[6];
-					//$obj->governingForm = $match[7]; unused
-				
-					$this->db->exec("INSERT INTO organizations (id, name, faction, num_members) VALUES (?, ?, ?, ?)", $obj->id, $obj->name, $obj->faction, $obj->num_members);
-				}
-			}
-			$this->db->commit();
-		} catch (\Exception $e) {
-			$this->logger->log("ERROR", "Error downloading orgs");
-			$this->db->rollback();
-		}
-		$this->logger->log("DEBUG", "Finished downloading orgs");
+		$this->logger->log("INFO", "Downloading all orgs from '$url'");
+			$searchIndex = 0;
+			$this->http
+				->get($url)
+				->withQueryParams(['l' => $this->searches[$searchIndex]])
+				->withTimeout(60)
+				->withCallback(function($response) use ($url, $searchIndex) {
+					$this->handleOrglistResponse($url, $searchIndex, $response);
+				});
 	}
 }
